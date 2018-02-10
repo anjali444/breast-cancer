7cdc5b0e6cd0328459f84db935f4aa857f46db99
anjali444 <32983796+anjali444@users.noreply.github.com>
2018-02-10 10:13:08 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+// http://www.bobrosslipsum.com/ Use this website for useless text, aka Lorem Ipsum. In the HTML, replace the '#' with the link to the new website and always fork this page. List your website resources in dedications.
+//American Cancer Society
+//breastcancer.org
+//Susan G Koman 
+//
